01b8c6d3ac0f5c16ca129cd49112660bed268ffb
Julien Dudek <julien.dudek@gmail.com>
2021-03-16 22:12:46 +0100

    ✨ appel firebase

--- app/src/main/java/fr/tuto/naturecollection/PlantRepository.kt
+++ app/src/main/java/fr/tuto/naturecollection/PlantRepository.kt
@@ -1,4 +1,45 @@
 package fr.tuto.naturecollection
 
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import fr.tuto.naturecollection.PlantRepository.Singleton.databaseRef
+import fr.tuto.naturecollection.PlantRepository.Singleton.plantList
+
 class PlantRepository {
+
+    object Singleton{
+        // se connecter à la réference plante
+        val databaseRef = FirebaseDatabase.getInstance().getReference("plants")
+
+        //créer une liste qui contient les plantes
+        val plantList = arrayListOf<PlantModel>()
+
+    }
+
+    fun updateData(){
+        // absorder les données récupérées et les mettre dans liste de plantes
+        databaseRef.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(p0: DataSnapshot) {
+                // retirer les anciennes plantes
+                plantList.clear()
+
+                // recolter la liste
+                for(ds in p0.children){
+                    val plant = ds.getValue(PlantModel::class.java)
+                    // verifier que la plante n'est pas null
+                    if(plant!= null){
+                        plantList.add(plant)
+                    }
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+
+        })
+    }
+
 }
--- app/src/main/java/fr/tuto/naturecollection/fragments/HomeFragment.kt
+++ app/src/main/java/fr/tuto/naturecollection/fragments/HomeFragment.kt
@@ -23,11 +23,6 @@ class HomeFragment(private val context:MainActivity) : Fragment() {
         // creer une liste qui stoke les plantes
         val planteList = arrayListOf<PlantModel>()
 
-        // ajout d'une plante
-        planteList.add(PlantModel("Pissenlit","Jaune soleil","https://cdn.pixabay.com/photo/2015/06/30/23/29/dandelion-827000_960_720.jpg",false))
-        planteList.add(PlantModel("Rose","Attention aux epines","https://cdn.pixabay.com/photo/2014/04/10/11/35/red-320891_960_720.jpg",false))
-        planteList.add(PlantModel("Cactus","Ça pique beaucoup","https://cdn.pixabay.com/photo/2014/07/29/08/55/cactus-404362_960_720.jpg",false))
-        planteList.add(PlantModel("Palmier","Rappel les vacances","https://cdn.pixabay.com/photo/2017/06/11/20/06/palm-2393391_960_720.jpg",false))
 
         // recuperer le ryceclerview
         val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
