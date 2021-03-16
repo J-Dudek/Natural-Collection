8bd7740ac9cc8fb295c2c96e9dc5d984e106f9e0
Julien Dudek <julien.dudek@gmail.com>
2021-03-16 15:23:22 +0100

    ✨ RecyclerView

---
+++ app/src/main/java/fr/tuto/naturecollection/adapter/PlantAdapter.kt
@@ -0,0 +1,34 @@
+package fr.tuto.naturecollection.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import fr.tuto.naturecollection.R
+
+class PlantAdapter: RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
+
+
+    // pour ranger tt les composants à controller
+    class ViewHolder(view:View): RecyclerView.ViewHolder(view){
+        val plantImage = view.findViewById<ImageView>(R.id.image_item)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_horizontal_plant,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 5
+    }
+    /** Ewactement pareil
+     * override fun getItemCount(): Int=5
+     */
+}
