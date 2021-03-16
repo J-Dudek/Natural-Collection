2bc03f585f57eb2a88ca6f32c0a3e9fb351af169
Julien Dudek <julien.dudek@gmail.com>
2021-03-16 14:18:30 +0100

    ✨ ajout premier fragment

--- app/src/main/java/fr/tuto/naturecollection/MainActivity.kt
+++ app/src/main/java/fr/tuto/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package fr.tuto.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fr.tuto.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Injecter le fragment home dans fragment_container
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,HomeFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
