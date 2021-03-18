3aafdf8a8a1f5758a6b62191f0fc097c0670d5e7
Julien Dudek <julien.dudek@gmail.com>
2021-03-19 00:28:30 +0100

    💄 filtre sur affichage plante

--- app/src/main/java/fr/tuto/naturecollection/fragments/HomeFragment.kt
+++ app/src/main/java/fr/tuto/naturecollection/fragments/HomeFragment.kt
@@ -24,7 +24,7 @@ class HomeFragment(private val context:MainActivity) : Fragment() {
 
         // recuperer le ryceclerview
         val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
-        horizontalRecyclerView.adapter = PlantAdapter(context,plantList, R.layout.item_horizontal_plant);
+        horizontalRecyclerView.adapter = PlantAdapter(context,plantList.filter { !it.liked }, R.layout.item_horizontal_plant);
 
         // recurer le second recycler view
         val verticalRecyclerView = view.findViewById<RecyclerView>(R.id.vertical_recycler_view)
